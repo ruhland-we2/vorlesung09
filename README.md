aa3519e0d87056db8113e6fd64820c33175393a6
hszg-ruhland <kruhland@hszg.de>
2022-06-09 13:12:00 +0200

    mit base58 decoding

--- README.md
+++ README.md
@@ -15,3 +15,35 @@ Ebenso wird eine Datei $.gitignore$ erstellt mit dem Inhalt
 ```
 node_modules
 ```
+
+Nach der Aktualisierung mit dem Repository muss
+```
+$npm update
+```
+
+aufgerufen werden.
+
+## Base58 Encoding
+
+Wegen des URL-Encodings in der Parameterübergabe wird das Base58 verwendet.
+D.h. meine Parameter werden zuerst in einen JSON-String gepackt
+
+```
+'{
+    "username": "klaus",
+    "password": "test123"
+
+}'
+```
+
+Dieser JSON-String wird mit Base58 kodiert 
+
+```
+knZmocde4thQX73CEfUucRfu4RC8Z46YjuwN5htCRYQ1jFazx9ToV7VVkixgsSzE73uRq4egUZi
+```
+
+und als Parameter übergeben.
+
+Im Restserver wird der String dann wieder Base58 decodiert. 
+Hierzu wird ein weiteres npm Paket genutzt: bs58. Aus mehreren Base58 Paketen wird dieses gewählt, da es am aktuellsten ist und die meisten Downloads besitzt
+
--- restserver.js
+++ restserver.js
@@ -2,17 +2,23 @@
 /* restserver.js 
  * npm i express
  * npm i helmet
+ * npm i bs58
  * NodeJS Restful Server, 03-2021
  * Klaus Ruhland, HSZG, Betriebssysteme II, Web Engineering II
  * Test with curl
- * 1.) curl  "http://localhost:8000/api/users?id=4&token=sdfa3&geo=us"
- * 2.) curl http://localhost:8000/api/test/12345
- * 3.) curl curl -d "param1=value1&param2=value2" -X POST http://localhost:8000/api/users
+ * 1.) curl  "http://localhost:8000/service/users?id=4&token=sdfa3&geo=us"
+ * 2.) curl http://localhost:8000/service/test/12345
+ * 3.) curl -d "param1=value1&param2=value2" -X POST http://localhost:8000/service/users
+ * 
+ * 2.) mit Base58 encoding
+ * curl http://localhost:8000/service/test-b58/79jdpA
+ * 
  */
 
 
 const express = require('express');
 const helmet = require('helmet');
+const bs58 = require('bs58');
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -24,18 +30,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routing Examples
-// http GET: http://localhost:8000/api/users?id=4&token=sdfa3&geo=us
+// http GET: http://localhost:8000/service/users?id=4&token=sdfa3&geo=us
 // Paremeter through ? in url 
-app.get('/api/users', function(req, res) {
+app.get('/service/users', function(req, res) {
     res.json(req.query);
 });
-// http GET: http://localhost:8000/api/test/12345
+// http GET: http://localhost:8000/service/test/12345
 // Parameter through path
-app.get('/api/test/:version', function(req, res) {
+app.get('/service/test/:version', function(req, res) {
     res.json(req.params);
 });
-// http POST: http://localhost:8000/api/users
-app.post('/api/users', function(req, res) {
+
+// http GET: curl http://localhost:8000/service/test-b58/79jdpA
+// Parameter through path with base58 decoding
+// 79jdpA is a base58 encoded 😏
+app.get('/service/test-b58/:b58version', function(req, res) {
+    let b58versionstring = req.params.b58version;
+    const bytes = bs58.decode(b58versionstring);
+    const version = Buffer.from(bytes).toString("utf8");
+    
+    let result = {
+        version: version
+    };
+
+    res.json(result);
+});
+
+// http POST: http://localhost:8000/service/users
+app.post('/service/users', function(req, res) {
     res.json(req.body);
 });
 
